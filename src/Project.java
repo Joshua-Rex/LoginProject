12c30efcdf315651c5d8a6d46441d6a2e411b5a0
Joshua-Rex <joshua.porterlightwood@gmail.com>
2023-09-01 15:06:18 +0100

    Reworked the program. Made a login class to handle the username checks and password checks. Then made a test application of the login class, this allowed for testing to make sure the methods worked.

--- src/Project.java
+++ src/Project.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class Project {
     public static void main(String[] args) {
-        // First set = Usernames | Second set = Passwords
-        // Username = Admin | Password = Password
-        String[][] accounts = { {"Admin", "password"}, {"Ash", "pikachu"}, {"Link", "zelda"}, {"Mario", "peach"}, {"Katarina", "garen"} };
+
 
         // Scanner for collecting inputs
         Scanner input = new Scanner(System.in);
+        Login l = new Login();
         // inputDetails stores the answer from the user, passHolder stores the password associated with the username
         String inputDetails;
-        String passHolder = "";
+        String passHolder;
 
         // breakOut confirms if the user entered a corrected username and password
         boolean breakOut = false;
@@ -20,17 +19,15 @@ public class Project {
         do {
             System.out.print("Enter username: ");
             inputDetails = input.nextLine();
-            for (String[] account : accounts) {
-                if (account[0].equals(inputDetails)) {
-                    breakOut = true;
-                    passHolder = account[1];
-                    break;
-                }
-            }
-        } while (!breakOut);
-
-        // Resets the breakOut variable for reuse in the next loop
-        breakOut = false;
+            passHolder = l.UsernameCheck(inputDetails);
+//            for (String[] account : accounts) {
+//                if (account[0].equals(inputDetails)) {
+//                    breakOut = true;
+//                    passHolder = account[1];
+//                    break;
+//                }
+//            }
+        } while (passHolder == null);
 
         // Gives the user 3 tries to correctly get the password for the account
         // If the user is successful, breaks out the loop and lets the user in
@@ -38,10 +35,9 @@ public class Project {
         for (int i = 0; i < 3; i++) {
             System.out.print("Enter Password: ");
             inputDetails = input.nextLine();
-            if (passHolder.equals(inputDetails.toLowerCase())) {
-                breakOut = true;
+            breakOut = l.PasswordCheck(passHolder, inputDetails);
+            if (breakOut)
                 break;
-            }
         }
 
         System.out.println(breakOut ? "Welcome..." : "ACCESS DENIED!");
